335ba7f52e2486dbb680cfa480ae235db102cca9
paolo <bifulco.paolo@gmail.com>
2020-08-14 20:20:16 -0700

    added parse options

--- README.md
+++ README.md
@@ -16,22 +16,10 @@ $ pip install -U -r requirements.txt
 * note: classes.names is in cfg folder
 
 
+## Data Info
+* approximately 1068 images 1080x1920
+* 56,047 individual worms
 
 ## Citation
 
 [![DOI](https://zenodo.org/badge/146165888.svg)](https://zenodo.org/badge/latestdoi/146165888)
-
-
-## About Us
-
-Ultralytics is a U.S.-based particle physics and AI startup with over 6 years of expertise supporting government, academic and business clients. We offer a wide range of vision AI services, spanning from simple expert advice up to delivery of fully customized, end-to-end production solutions, including:
-- **Cloud-based AI** systems operating on **hundreds of HD video streams in realtime.**
-- **Edge AI** integrated into custom iOS and Android apps for realtime **30 FPS video inference.**
-- **Custom data training**, hyperparameter evolution, and model exportation to any destination.
-
-For business inquiries and professional support requests please visit us at https://www.ultralytics.com.
-
-
-## Contact
-
-**Issues should be raised directly in the repository.** For business inquiries or professional support requests please visit https://www.ultralytics.com or email Glenn Jocher at glenn.jocher@ultralytics.com.
--- yolov3_tracker_latest.py
+++ yolov3_tracker_latest.py
@@ -35,20 +35,23 @@ def draw_on_im(img, x1, y1, x2, y2, conf, col):
     )
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=1, help="size of each image batch")
     parser.add_argument("--model_def", type=str, default="cfg/yolov3-spp-1cls.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="old_608/best.pt", help="path to weights file")
+    parser.add_argument("--weights_path", type=str, default="weights/best.pt", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="cfg/classes.names", help="path to class label file")
     parser.add_argument("--iou_thres", type=float, default=0.6, help="iou threshold required to qualify as detected def 0.4")
     parser.add_argument("--conf_thres", type=float, default=0.3, help="object confidence threshold def 0.001")
-    #parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression def 0.5")
     parser.add_argument("--no_gpu", default=True, help="cuda enabled gpu")
-    parser.add_argument("--img_size", type=int, default=608, help="size of each image dimension of firts layer of yolo")
+    parser.add_argument("--img_size", type=int, default=608, help="size of each image dimension of first layer of yolo")
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
+
+    parser.add_argument('--out_path', type=str, default='output/custom/')
+    parser.add_argument("--data_path", type=str, default='data/samples' help="path the video file or to the directory with stack of images")
+    parser.add_argument("--video", action='store_true', help="save the labled images as a video")
+    parser.add_argument("--csv", action='store_true', help="save the bounding box data into a csv in the out directory")
     opt = parser.parse_args()
 
     # create settings dictionaryprint(input_img.shape)
@@ -63,6 +66,14 @@ if __name__ == "__main__":
                 'augment': opt.augment,
                 'classes': opt.classes}
 
+
+    # determine input type
+    INPUT_VIDEO = bool
+    if ".avi" in opt.data_path:
+        INPUT_VIDEO = True
+        print("\n Input being processed as a video \n")
+
+
     # load yolov3 model
     yolo = YoloModelLatest(settings)
 
@@ -72,7 +83,7 @@ if __name__ == "__main__":
         x=0
         for frame in frames:
             print(frame.shape)
-            img_obj = ImageProcessor(frame)
+            img_obj = ImageProcessor(frame, out_size=416)
             input_dict = img_obj.image_slices
             # input is in format [image, position]
             # it breaks on the last frame not sure a good work around
